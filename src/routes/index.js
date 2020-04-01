d61df4ec5568619c72d9d66899785efd20e27ec1
RKRafaelNascimento <RKRafael.nascimento@gmail.com>
2020-04-01 19:07:06 -0300

    Rotas Privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,19 @@
-import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
+import SignUp from '../pages/SignUp';
+import SignIn from '../pages/SignIn';
+import Dashboard from '../pages/Dashboard';
+import Profile from '../pages/Profile';
 
-import SignUp from '../pages/SignUp'
-import SignIn from '../pages/SignIn'
-import Dashboard from '../pages/Dashboard'
-import Profile from '../pages/Profile'
-
-
-export default function Routes () {
-    return(
+export default function Routes() {
+    return (
         <Switch>
-            <Route path="/" exact component={SignIn}/>
-            <Route path="/register" component={SignUp}/>
+            <Route path="/" exact component={SignIn} />
+            <Route path="/register" component={SignUp} />
 
-            <Route path="/dashboard" component={Dashboard}/>
-            <Route path="/profile" component={Profile}/>
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/profile" component={Profile} isPrivate />
         </Switch>
-    )
+    );
 }
